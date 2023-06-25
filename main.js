4d975b7478068aeefb6e0c8b2c03a3e379506c05
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-06-25 16:18:33 +0200

    Create a recursive Fibonacci function

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ const fibs = function fibs(desiredLength) {
   const result = [];
   for (let i = 0; i < desiredLength; i++) {
     const newNumber = first + second;
-    first = second;
-    second = newNumber;
-
-    result.push(newNumber);
+    if (i === 0) {
+      result.push(first);
+    } else if (i === 1) {
+      result.push(second);
+    } else {
+      first = second;
+      second = newNumber;
+      result.push(newNumber);
+    }
   }
 
   return result;
@@ -16,3 +21,20 @@ const fibs = function fibs(desiredLength) {
 
 console.log(fibs(7));
 console.log(fibs(40));
+
+const fibsRec = function fibsRec(desiredLength) {
+  if (desiredLength === 1) {
+    return [0];
+  }
+  if (desiredLength === 2) {
+    const array = fibsRec(desiredLength - 1);
+    array.push(1);
+    return array;
+  }
+  const newArray = fibsRec(desiredLength - 1);
+  newArray.push(newArray[newArray.length - 2] + newArray[newArray.length - 1]);
+  return newArray;
+};
+
+console.log(fibsRec(7));
+console.log(fibsRec(40));
